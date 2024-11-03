74d25cb9764274a9a09ee453f7a1024049e25e66
Prasad-P-G <prasad.dwd@gmail.com>
2024-11-03 17:11:13 +0530

    changed local port to 8080

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
